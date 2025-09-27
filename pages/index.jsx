746c92f6c311ef470487d5a04b05606a37f7695b
Kaglioster <davide.marzetti@gmail.com>
2025-09-27 06:02:47 +0200

    fix(build): tailwind globals + clean index.jsx + import order

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,1 +1,25 @@
-C:\Users\KR25
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Servizi from "../components/Servizi";
+import AdrEuropa from "../components/AdrEuropa";
+import Pubblicazioni from "../components/Pubblicazioni";
+import Modulistica from "../components/Modulistica";
+import Contatti from "../components/Contatti";
+import Footer from "../components/Footer";
+
+export default function Home(){
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <main className="container">
+        <section id="servizi" className="section"><Servizi /></section>
+        <section id="adr" className="band band--dark"><AdrEuropa /></section>
+        <section id="pubblicazioni" className="band band--light"><Pubblicazioni /></section>
+        <section id="modulistica" className="section"><Modulistica /></section>
+        <section id="contatti" className="section"><Contatti /></section>
+      </main>
+      <Footer />
+    </>
+  );
+}
